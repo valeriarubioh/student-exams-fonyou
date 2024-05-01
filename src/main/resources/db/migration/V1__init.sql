d47aaa03fbcb7ba526d7382bbc40a6751b15faec
Valeria Rubio <valeriarubioh@gmail.com>
2024-05-01 17:05:04 -0500

    Created initial database schema

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,59 @@
+-- Create the students table
+CREATE TABLE students (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    city_time_zone VARCHAR(50) NOT NULL
+);
+
+-- Create the exams table
+CREATE TABLE exams (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    exam_name VARCHAR(100) NOT NULL,
+    total_score INT NOT NULL, -- 100 points always
+    exam_time_zone VARCHAR(50) NOT NULL DEFAULT 'UTC-5'
+);
+
+-- Create the questions table
+CREATE TABLE questions (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    exam_id BIGINT NOT NULL,
+    question TEXT NOT NULL,
+    choice1 TEXT NOT NULL,
+    choice2 TEXT NOT NULL,
+    choice3 TEXT NOT NULL,
+    choice4 TEXT NOT NULL,
+    correct_choice INT NOT NULL, -- 1 to 4
+    score INT NOT NULL, -- 10
+    FOREIGN KEY (exam_id) REFERENCES exams(id)
+);
+
+-- Create the exam_schedules table
+CREATE TABLE exam_schedules (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    exam_id BIGINT NOT NULL,
+    student_id BIGINT NOT NULL,
+    start_date_time DATETIME NOT NULL,
+    end_date_time DATETIME NOT NULL,
+    FOREIGN KEY (exam_id) REFERENCES exams(id),
+    FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+-- Create the student_responses table
+CREATE TABLE student_responses (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    exam_schedule_id BIGINT NOT NULL,
+    question_id BIGINT NOT NULL,
+    student_response INT NOT NULL,
+    FOREIGN KEY (exam_schedule_id) REFERENCES exam_schedules(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+-- Create the exam_grades table
+CREATE TABLE exam_grades (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    exam_schedule_id BIGINT NOT NULL,
+    grade INT NOT NULL,
+    FOREIGN KEY (exam_schedule_id) REFERENCES exam_schedules(id)
+);
